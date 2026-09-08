6f9a1e72e0e57c993b3b3d08cf3ded8502c05ca4
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2026-09-09 02:31:18 +0900

    fix: distはだめ

--- dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-export { GiracleApiError } from "./errors";
-export { GiracleClient } from "./client";
-export { GiracleSocket, nextBackoffMs, WS_OPEN } from "./ws";
-export { GiracleBot } from "./bot";
-export type { BotEventMap, BotOptions, EditMessageInput, EditResult, InboxAdded, Message, SendMessageInput, SignalEnvelope, } from "./types";
